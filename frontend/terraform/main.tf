a539e652289d98f9b086af3da5ceb2f922e64491
Stephanie Chester <schester81@gmail.com>
2023-04-18 19:21:42 -0400

    cloudfront file moved to main.tf

--- frontend/terraform/main.tf
+++ frontend/terraform/main.tf
@@ -12,3 +12,115 @@ terraform {
     }
   }
 }
+
+# Cloudfront distribution for main s3 site.
+resource "aws_cloudfront_distribution" "www_s3_distribution" {
+  origin {
+    domain_name = "www.stephaniechester.com.s3-website-us-east-1.amazonaws.com"
+    origin_id   = "${var.subdomain}"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = [var.subdomain]
+
+  custom_error_response {
+    error_caching_min_ttl = 0
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/404.html"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.s3_bucket_names[1]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 31536000
+    default_ttl            = 31536000
+    max_ttl                = 31536000
+    compress               = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.my_cert_validate.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
+  }
+}
+
+# Cloudfront S3 for redirect to www.
+resource "aws_cloudfront_distribution" "root_s3_distribution" {
+  origin {
+    domain_name = "${var.domain_name_root}"
+    origin_id   = "${var.domain_name_root}"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  aliases = ["var.domain_name_root"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.s3_bucket_names[0]
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+
+      headers = ["Origin"]
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.my_cert_validate.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
+  }
+}
